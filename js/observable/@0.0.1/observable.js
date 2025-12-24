4e06c736ae7c57b35e902f24511cf9869e19f08a
Sean Robertson <srobertson421@gmail.com>
2025-12-24 10:57:45 -0800

    Update observable.js

--- js/observable/@0.0.1/observable.js
+++ js/observable/@0.0.1/observable.js
@@ -1,7 +1,7 @@
 class Observable {
   constructor(value) {
     this._value = value;
-    this._subs = {};
+    this._subs = new Map();
   }
 
   get value() {
@@ -9,20 +9,43 @@ class Observable {
   }
 
   set value(newValue) {
+    // Skip if value hasn't actually changed
+    if (newValue === this._value) return;
+    
     const oldValue = this._value;
     this._value = newValue;
-    Object.getOwnPropertySymbols(this._subs).forEach(symKey => {
-      this._subs[symKey](this._value, oldValue);
+    
+    // Notify all subscribers with error isolation
+    this._subs.forEach(callback => {
+      try {
+        callback(this._value, oldValue);
+      } catch (err) {
+        console.error('Observable subscriber error:', err);
+      }
     });
   }
 
-  subscribe(cb) {
+  subscribe(callback, immediate = false) {
     const sym = Symbol();
-    this._subs[sym] = cb;
-
-    return () => {
-      delete this._subs[sym];
+    this._subs.set(sym, callback);
+    
+    // Optionally invoke callback immediately with current value
+    if (immediate) {
+      try {
+        callback(this._value, this._value);
+      } catch (err) {
+        console.error('Observable subscriber error:', err);
+      }
     }
+    
+    // Return unsubscribe function
+    return () => {
+      this._subs.delete(sym);
+    };
+  }
+
+  dispose() {
+    this._subs.clear();
   }
 }
 
